1b843110c5dc4b759efa40db6fc7b1948c5f76d8
Amine <bendahmane.amine@gmail.com>
2024-11-29 20:21:03 +0100

    add Nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom"
+
+const Nav = ({authedUser}) => {
+    return (
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        {
+          authedUser === null ? 
+            <NavLink to="/login">Login</NavLink> :
+            <>
+              <NavLink to="/add">Add Question</NavLink>
+              <NavLink to="/leaderboard">Leaderboard</NavLink>
+              <NavLink to={{ pathname: "/login", search: "?action=logout" }}>Logout</NavLink>
+            </>
+        }
+      </nav>
+    );
+};
+  
+export default Nav;
