8e14341ba7256844240946f421ee04a92e7cb6d0
Amine <bendahmane.amine@gmail.com>
2024-11-29 20:24:19 +0100

    add RequireAuth to protect routes

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,34 @@ import Leaderboard from './Leaderboard';
 import AddQuestion from './AddQuestion';
 import Question from './Question';
 import Error404 from './Error404';
+import Nav from './Nav.js';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const App = (props) => {
+  const [authedUser, setAuthedUser] = useState("mtsamis"); //TODO: remove later
+
+  function RequireAuth({ children }) {
+    const location = useLocation();
+    return authedUser !== null ? children : 
+      <Navigate to="/login" replace state={{ path: location.pathname }} />;
+  }
+  
   return (
     <div className="App">
-      {
+      <Nav authedUser={authedUser} />
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/login" exact element={<Login />} />
-          <Route path="/leaderboard" exact element={<Leaderboard />} />
-          <Route path="/add" exact element={<AddQuestion />} />
-          <Route path="/questions/:id" element={<Question />} />
+          <Route path="/leaderboard" exact 
+            element={<RequireAuth><Leaderboard /></RequireAuth>} />
+          <Route path="/add" exact 
+            element={<RequireAuth><AddQuestion /></RequireAuth>} />
+          <Route path="/questions/:id" 
+            element={<RequireAuth><Question /></RequireAuth>} />
           <Route path="*" element={<Error404 />} />
         </Routes>
-      }
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 const Login = (props) => {
+    const location = useLocation();
+    //TODO: redirect to location.state["path"] when logged in
     return <div>
+        {
+            location.state !== null && 
+                <p>Please login to access this page.</p>
+        }
         Login
-        </div>;
+    </div>;
 };
 
 export default Login;
