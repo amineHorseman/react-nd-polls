e222076790428a603fdbdabb18a7ffd837628292
Amine <bendahmane.amine@gmail.com>
2024-11-30 06:52:03 +0100

    Add Error404 page content

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error404 = (props) => {
-    return <div>
-        Error 404
-        </div>;
+    return <div className="center">
+        <h1>Error 404</h1>
+        <p>The page you're looking for does not exist.</p>
+        <Link to="/">Return to homepage</Link>
+    </div>;
 };
 
 export default Error404;
