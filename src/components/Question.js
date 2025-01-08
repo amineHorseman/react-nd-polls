b601739dbd4f816c3a1358795bde8382d1575349
Amine <bendahmane.amine@gmail.com>
2025-01-08 08:11:35 +0100

    add Question component logic

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,34 @@
-const Question = (props) => {
-    return <div>
-        Question
-        </div>;
+import { selectQuestion } from "../features/questions/questionsSlice";
+import { selectUser } from "../features/users/usersSlice";
+import { displayAvatar, displayElapsedTime } from "../utils/helpers";
+import { useSelector } from "react-redux";
+
+const Question = ({id}) => {
+    const question = useSelector(state => selectQuestion(state, id));
+    const author = useSelector(state => selectUser(state, question.author));
+
+    return (
+        <div className="card shadow-sm">
+            <div className="card-header bg-light">
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="d-flex align-items-center">
+                        { displayAvatar(author["avatarURL"]) }
+                        <span className="ms-2">{author["id"]}</span>
+                    </div>
+                    <small className="text-muted">
+                        Asked { displayElapsedTime(question["timestamp"]) }
+                    </small>
+                </div>
+            </div>
+            <div className="card-body">
+                <h5 className="card-title text-center mb-4">Would You Rather</h5>
+                <div className="d-grid gap-2">
+                    <button className="btn btn-outline-primary">{question.optionOne.text}</button>
+                    <button className="btn btn-outline-primary">{question.optionTwo.text}</button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Question;
