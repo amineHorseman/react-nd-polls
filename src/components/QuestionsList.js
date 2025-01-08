ba33249c621f02910abf4b8dcf7e988c051f73e8
Amine <bendahmane.amine@gmail.com>
2025-01-08 07:44:32 +0100

    add QuestionsList component logic

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -1,7 +1,29 @@
+import { selectAllQuestions } from "../features/questions/questionsSlice";
+import { selectAuthedUserId } from "../features/auth/authSlice";
+import { useSelector } from "react-redux";
+import Question from "./Question";
+
 const QuestionsList = () => {
-    return <div>
-        Questions List
-        </div>;
+    const questions = useSelector(selectAllQuestions);
+    const username = useSelector(selectAuthedUserId);
+        
+    const unvotedQuestions = Object.keys(questions)
+        .filter((id) => (
+            questions[id].optionOne.votes.indexOf(username) === -1 && 
+            questions[id].optionTwo.votes.indexOf(username) === -1));
+        
+    return (
+        <div className="container">
+            <div className="row g-4">
+                {
+                    unvotedQuestions.map((id) => (
+                        <div key={id}>
+                            <Question id={id} />
+                        </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default QuestionsList;
