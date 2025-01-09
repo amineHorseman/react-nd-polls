042e87a5ff25671bcb9c110b58761e5512564f84
Amine <bendahmane.amine@gmail.com>
2025-01-09 07:27:19 +0100

    add different Question layout for voted Questions

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,10 +3,17 @@ import { selectUser } from "../features/users/usersSlice";
 import { displayAvatar, displayElapsedTime } from "../utils/helpers";
 import { useSelector } from "react-redux";
 
-const Question = ({id}) => {
+const Question = ({id, voted}) => {
     const question = useSelector(state => selectQuestion(state, id));
     const author = useSelector(state => selectUser(state, question.author));
 
+    let userChoice = question.optionOne.text;
+    let otherChoice = question.optionTwo.text;
+    if (voted && author["answers"]["id"] === 'optionTwo') {
+        userChoice = question.optionTwo.text;
+        otherChoice = question.optionOne.text;
+    }
+    
     return (
         <div className="card shadow-sm">
             <div className="card-header bg-light">
@@ -21,11 +28,23 @@ const Question = ({id}) => {
                 </div>
             </div>
             <div className="card-body">
-                <h5 className="card-title text-center mb-4">Would You Rather</h5>
-                <div className="d-grid gap-2">
-                    <button className="btn btn-outline-primary">{question.optionOne.text}</button>
-                    <button className="btn btn-outline-primary">{question.optionTwo.text}</button>
-                </div>
+                <h5 className="card-title text-center mb-4">
+                    { voted? 'You Would Rather' : 'Would You Rather'}
+                </h5>
+                {
+                        voted ? (
+                            <div className="d-grid gap-2">
+                                <p className="btn btn-primary btn-static">{userChoice}</p>
+                                <h5>Than
+                                </h5>
+                                <button className="btn btn-outline-danger" disabled>{otherChoice}</button>
+                            </div>
+                        ) : (
+                            <div className="d-grid gap-2">
+                                <button className="btn btn-outline-primary">{question.optionOne.text}</button>
+                                <button className="btn btn-outline-primary">{question.optionTwo.text}</button>
+                            </div>)
+                }
             </div>
         </div>
     );
