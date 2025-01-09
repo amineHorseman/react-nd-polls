9ca449ce5a58fe964de4655f103df9e9763dd216
Amine <bendahmane.amine@gmail.com>
2025-01-09 07:24:05 +0100

    add questions filtering toggle button in homepage

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -1,24 +1,45 @@
 import { selectAllQuestions } from "../features/questions/questionsSlice";
 import { selectAuthedUserId } from "../features/auth/authSlice";
+import { selectUser } from "../features/users/usersSlice";
 import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import Question from "./Question";
 
 const QuestionsList = () => {
     const questions = useSelector(selectAllQuestions);
     const username = useSelector(selectAuthedUserId);
-        
+    const user = useSelector(state => selectUser(state, username));
+    const [votedFilter, setVotedFilter] = useState(false);
+    const [displayedQuestions, setDisplayedQuestions] = useState([]);
+
+    const votedQuestions = Object.keys(questions)
+        .filter((id) => id in user.answers);
     const unvotedQuestions = Object.keys(questions)
-        .filter((id) => (
-            questions[id].optionOne.votes.indexOf(username) === -1 && 
-            questions[id].optionTwo.votes.indexOf(username) === -1));
-        
+        .filter((id) => !(id in user.answers));
+
+    useEffect(() => {
+        votedFilter ? setDisplayedQuestions(votedQuestions) :
+            setDisplayedQuestions(unvotedQuestions)
+    }, [votedFilter]);
+
+    function toggleVoted(e) {
+        setVotedFilter(e.target.checked);
+    }
+
     return (
         <div className="container">
+            <div className="d-flex justify-content-end gap-3 mb-3">
+                <span className="form-check form-switch">
+                    <input className="form-check-input" type="checkbox" role="switch"
+                        id="check-voted-filter" onChange={toggleVoted} checked={votedFilter} />
+                    <label className="form-check-label" htmlFor="check-voted-filter">Show my past answers</label>
+                </span>
+            </div>
             <div className="row g-4">
                 {
-                    unvotedQuestions.map((id) => (
+                    displayedQuestions.map((id) => (
                         <div key={id}>
-                            <Question id={id} />
+                            <Question id={id} voted={votedFilter} />
                         </div>
                 ))}
             </div>
