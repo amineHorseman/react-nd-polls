a1237df26403d89d76b18012f11e0b2be4256d56
Amine <bendahmane.amine@gmail.com>
2025-01-12 11:17:48 +0100

    Fix voted answers bug and improve elements style

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,3 +1,4 @@
+import { selectAuthedUserId } from "../features/auth/authSlice";
 import { selectQuestion } from "../features/questions/questionsSlice";
 import { selectUser } from "../features/users/usersSlice";
 import { displayAvatar, displayElapsedTime } from "../utils/helpers";
@@ -6,16 +7,18 @@ import { useSelector } from "react-redux";
 const Question = ({id, voted}) => {
     const question = useSelector(state => selectQuestion(state, id));
     const author = useSelector(state => selectUser(state, question.author));
+    const authedUserId = useSelector(selectAuthedUserId);
+    const authedUser = useSelector(state => selectUser(state, authedUserId));
 
     let userChoice = question.optionOne.text;
     let otherChoice = question.optionTwo.text;
-    if (voted && author["answers"]["id"] === 'optionTwo') {
+    if (voted && authedUser["answers"][id] === 'optionTwo') {
         userChoice = question.optionTwo.text;
         otherChoice = question.optionOne.text;
     }
     
     return (
-        <div className="card shadow-sm">
+        <div className="card shadow-sm question">
             <div className="card-header bg-light">
                 <div className="d-flex justify-content-between align-items-center">
                     <div className="d-flex align-items-center">
@@ -28,16 +31,15 @@ const Question = ({id, voted}) => {
                 </div>
             </div>
             <div className="card-body">
-                <h5 className="card-title text-center mb-4">
+                <h5 className="card-title text-center mb-3">
                     { voted? 'You Would Rather' : 'Would You Rather'}
                 </h5>
                 {
                         voted ? (
-                            <div className="d-grid gap-2">
+                            <div className="d-grid col-6 gap-2 mx-auto">
                                 <p className="btn btn-primary btn-static">{userChoice}</p>
-                                <h5>Than
-                                </h5>
-                                <button className="btn btn-outline-danger" disabled>{otherChoice}</button>
+                                <h5 className="mb-1">Than</h5>
+                                <p className="btn btn-outline-danger btn-static">{otherChoice}</p>
                             </div>
                         ) : (
                             <div className="d-grid gap-2">
